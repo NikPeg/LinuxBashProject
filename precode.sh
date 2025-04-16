91c1975ceadd3d6d00ee4427771ce2808c872d8c
NikPeg <peganov.nik@gmail.com>
2025-04-16 16:57:18 +0300

    name

--- precode.sh
+++ precode.sh
@@ -34,11 +34,14 @@ cp -r dir2 dir3/dir4
 find . -name "*.txt" > dir1/summary.txt
 
 # дописываем в task/dir1/summary.txt содержимое task/dir2/list.txt
+cat dir2/list.txt >> dir1/summary.txt
 
 # определяем переменную окружения NAME со значением "Всем студентам"
+NAME='Всем студентам'
 
 # запускаем task/dir2/hello.sh с переменной окружения NAME в качестве аргумента
 # вывод скрипта должен дописаться в файл task/dir1/summary.txt
+./dir2/hello.sh "$NAME" >> dir1/summary.txt
 
 # перемещаем с переименованием task/dir1/summary.txt в task/Практическое задание
 
